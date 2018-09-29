8e9e93e4db3b8397755beedd20d3b3416c5dc929
lzysoo <xinqing_lzy@126.com>
2018-09-29 18:46:31 +0800

    1

---
+++ src/practices/written/SmallGame.java
@@ -0,0 +1,16 @@
+package practices.written;
+
+import java.util.Scanner;
+
+public class SmallGame {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//        int t = Integer.parseInt(sc.nextLine());
+//        int[] arr = new int[t];
+//        for (int i = 0; i < t; i++) {
+//            arr[i] =
+//        }
+
+    }
+}
